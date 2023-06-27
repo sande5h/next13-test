d239d7c46b06020eb8e2ab731e3ad972f3df5b33
sande5h <bhusalsandesh0@gmail.com>
2023-06-28 04:18:53 +0545

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-const Home =() =>{
-    return(
-        <div> <h1>Home</h1></div>
-    )
-
-}
+const Home = () => {
+  return (
+    <section className="flexStart flex-col paddings mb-16">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>More</h1>
+    </section>
+  );
+};
 
 export default Home;
